7ac8ec995f2fc784937d948da296cda553a4845b
Pedro Barreiro <pedro.barreiro@farfetch.com>
2021-12-03 17:38:53 +0000

    chore(client|redux|react): profile - rename API and  client split
    This renames the API according to the next version naming system.
    It also splits  the client into isolated clients and redux isolated actions.

---
+++ packages/redux/src/users/actionTypes.js
@@ -0,0 +1,155 @@
+/**
+ * @module users/actionTypes
+ * @category Users
+ * @subcategory Actions
+ */
+
+/** Action type dispatched when the get users request fails. */
+export const FETCH_USER_FAILURE =
+  '@farfetch/blackout-client/FETCH_USER_FAILURE';
+/** Action type dispatched when the get users request starts. */
+export const FETCH_USER_REQUEST =
+  '@farfetch/blackout-client/FETCH_USER_REQUEST';
+/** Action type dispatched when the get users request succeeds. */
+export const FETCH_USER_SUCCESS =
+  '@farfetch/blackout-client/FETCH_USER_SUCCESS';
+
+/** Action type dispatched when the update users request fails. */
+export const UPDATE_USER_FAILURE =
+  '@farfetch/blackout-client/UPDATE_USER_FAILURE';
+/** Action type dispatched when the update users request starts. */
+export const UPDATE_USER_REQUEST =
+  '@farfetch/blackout-client/UPDATE_USER_REQUEST';
+/** Action type dispatched when the update users§ request succeeds. */
+export const UPDATE_USER_SUCCESS =
+  '@farfetch/blackout-client/UPDATE_USER_SUCCESS';
+
+/** Action type dispatched when the get benefits request fails. */
+export const FETCH_BENEFITS_FAILURE =
+  '@farfetch/blackout-client/FETCH_BENEFITS_FAILURE';
+/** Action type dispatched when the get benefits request starts. */
+export const FETCH_BENEFITS_REQUEST =
+  '@farfetch/blackout-client/FETCH_BENEFITS_REQUEST';
+/** Action type dispatched when the get benefits request succeeds. */
+export const FETCH_BENEFITS_SUCCESS =
+  '@farfetch/blackout-client/FETCH_BENEFITS_SUCCESS';
+
+/** Action type dispatched when the get preferences request fails. */
+export const FETCH_PREFERENCES_FAILURE =
+  '@farfetch/blackout-client/FETCH_PREFERENCES_FAILURE';
+/** Action type dispatched when the get preferences request starts. */
+export const FETCH_PREFERENCES_REQUEST =
+  '@farfetch/blackout-client/FETCH_PREFERENCES_REQUEST';
+/** Action type dispatched when the get preferences request succeeds. */
+export const FETCH_PREFERENCES_SUCCESS =
+  '@farfetch/blackout-client/FETCH_PREFERENCES_SUCCESS';
+
+/** Action type dispatched when the update preferences request fails. */
+export const UPDATE_PREFERENCES_FAILURE =
+  '@farfetch/blackout-client/UPDATE_PREFERENCES_FAILURE';
+/** Action type dispatched when the update preferences request starts. */
+export const UPDATE_PREFERENCES_REQUEST =
+  '@farfetch/blackout-client/UPDATE_PREFERENCES_REQUEST';
+/** Action type dispatched when the update preferences request succeeds. */
+export const UPDATE_PREFERENCES_SUCCESS =
+  '@farfetch/blackout-client/UPDATE_PREFERENCES_SUCCESS';
+
+/** Action type dispatched when the get titles request fails. */
+export const FETCH_TITLES_FAILURE =
+  '@farfetch/blackout-client/FETCH_TITLES_FAILURE';
+/** Action type dispatched when the get titles request starts. */
+export const FETCH_TITLES_REQUEST =
+  '@farfetch/blackout-client/FETCH_TITLES_REQUEST';
+/** Action type dispatched when the get titles request succeeds. */
+export const FETCH_TITLES_SUCCESS =
+  '@farfetch/blackout-client/FETCH_TITLES_SUCCESS';
+
+/** Action type dispatched when the get credit request fails. */
+export const FETCH_CREDIT_FAILURE =
+  '@farfetch/blackout-client/FETCH_CREDIT_FAILURE';
+/** Action type dispatched when the get credit request starts. */
+export const FETCH_CREDIT_REQUEST =
+  '@farfetch/blackout-client/FETCH_CREDIT_REQUEST';
+/** Action type dispatched when the get credit request succeeds. */
+export const FETCH_CREDIT_SUCCESS =
+  '@farfetch/blackout-client/FETCH_CREDIT_SUCCESS';
+
+/** Action type dispatched when the get contact request fails. */
+export const FETCH_CONTACT_FAILURE =
+  '@farfetch/blackout-client/FETCH_CONTACT_FAILURE';
+/** Action type dispatched when the get contact request starts. */
+export const FETCH_CONTACT_REQUEST =
+  '@farfetch/blackout-client/FETCH_CONTACT_REQUEST';
+/** Action type dispatched when the get contact request succeeds. */
+export const FETCH_CONTACT_SUCCESS =
+  '@farfetch/blackout-client/FETCH_CONTACT_SUCCESS';
+
+/** Action type dispatched when the create contact request fails. */
+export const CREATE_CONTACT_FAILURE =
+  '@farfetch/blackout-client/CREATE_CONTACT_FAILURE';
+/** Action type dispatched when the create contact request starts. */
+export const CREATE_CONTACT_REQUEST =
+  '@farfetch/blackout-client/CREATE_CONTACT_REQUEST';
+/** Action type dispatched when the create contact request succeeds. */
+export const CREATE_CONTACT_SUCCESS =
+  '@farfetch/blackout-client/CREATE_CONTACT_SUCCESS';
+
+/** Action type dispatched when the update contact request fails. */
+export const UPDATE_CONTACT_FAILURE =
+  '@farfetch/blackout-client/UPDATE_CONTACT_FAILURE';
+/** Action type dispatched when the update contact request starts. */
+export const UPDATE_CONTACT_REQUEST =
+  '@farfetch/blackout-client/UPDATE_CONTACT_REQUEST';
+/** Action type dispatched when the update contact request succeeds. */
+export const UPDATE_CONTACT_SUCCESS =
+  '@farfetch/blackout-client/UPDATE_CONTACT_SUCCESS';
+
+/** Action type dispatched when the delete contact request fails. */
+export const DELETE_CONTACT_FAILURE =
+  '@farfetch/blackout-client/DELETE_CONTACT_FAILURE';
+/** Action type dispatched when the delete contact request starts. */
+export const DELETE_CONTACT_REQUEST =
+  '@farfetch/blackout-client/DELETE_CONTACT_REQUEST';
+/** Action type dispatched when the delete contact request succeeds. */
+export const DELETE_CONTACT_SUCCESS =
+  '@farfetch/blackout-client/DELETE_CONTACT_SUCCESS';
+
+/** Action type dispatched when the get contacts request fails. */
+export const FETCH_CONTACTS_FAILURE =
+  '@farfetch/blackout-client/FETCH_CONTACTS_FAILURE';
+/** Action type dispatched when the get contacts request starts. */
+export const FETCH_CONTACTS_REQUEST =
+  '@farfetch/blackout-client/FETCH_CONTACTS_REQUEST';
+/** Action type dispatched when the get contacts request succeeds. */
+export const FETCH_CONTACTS_SUCCESS =
+  '@farfetch/blackout-client/FETCH_CONTACTS_SUCCESS';
+
+/** Action type dispatched when the get credit movements request fails. */
+export const FETCH_CREDIT_MOVEMENTS_FAILURE =
+  '@farfetch/blackout-client/FETCH_CREDIT_MOVEMENTS_FAILURE';
+/** Action type dispatched when the get credit movements request starts. */
+export const FETCH_CREDIT_MOVEMENTS_REQUEST =
+  '@farfetch/blackout-client/FETCH_CREDIT_MOVEMENTS_REQUEST';
+/** Action type dispatched when the get credit movements request succeeds. */
+export const FETCH_CREDIT_MOVEMENTS_SUCCESS =
+  '@farfetch/blackout-client/FETCH_CREDIT_MOVEMENTS_SUCCESS';
+
+/** Action type dispatched when the create guest user request fails. */
+export const CREATE_GUEST_USER_FAILURE =
+  '@farfetch/blackout-client/CREATE_GUEST_USER_FAILURE';
+/** Action type dispatched when the create guest user request starts. */
+export const CREATE_GUEST_USER_REQUEST =
+  '@farfetch/blackout-client/CREATE_GUEST_USER_REQUEST';
+/** Action type dispatched when the create guest user request succeeds. */
+export const CREATE_GUEST_USER_SUCCESS =
+  '@farfetch/blackout-client/CREATE_GUEST_USER_SUCCESS';
+
+/** Action type dispatched when the get guest user request fails. */
+export const FETCH_GUEST_USER_FAILURE =
+  '@farfetch/blackout-client/FETCH_GUEST_USER_FAILURE';
+/** Action type dispatched when the get guest user request starts. */
+export const FETCH_GUEST_USER_REQUEST =
+  '@farfetch/blackout-client/FETCH_GUEST_USER_REQUEST';
+/** Action type dispatched when the get guest user request succeeds. */
+export const FETCH_GUEST_USER_SUCCESS =
+  '@farfetch/blackout-client/FETCH_GUEST_USER_SUCCESS';
